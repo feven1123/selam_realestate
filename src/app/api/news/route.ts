1efe60a6904c65ed4a28c0b569219bd089c2d9cf
feven1123 <favumail20@gmail.com>
2025-07-31 10:59:24 +0300

    re

--- src/app/api/news/route.ts
+++ src/app/api/news/route.ts
@@ -25,7 +25,7 @@ export async function POST(request: Request) {
   }
 }
 
-export async function GET(request: Request) {
+export async function GET() {
   try {
     const news = await prisma.news.findMany({
       orderBy: { publishedAt: 'desc' },
